e09417bcc357d5be2fccc097ac6c947dfe95961f
William S Chen <blackjamx5@hotmail.com>
2019-04-01 12:38:28 -0700

    First Real Commit

---
+++ python3/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/Work/Documents/College/Senior/S2/Databases/Final Project/Project Files/python3/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
