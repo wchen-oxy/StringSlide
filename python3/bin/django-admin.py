6fa1e27094508fa8c030cf3731f18833a8c661c9
William S Chen <blackjamx5@hotmail.com>
2019-04-13 15:47:50 -0700

    removed python3

--- python3/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/Work/Documents/College/Senior/S2/Databases/Final Project/Project Files/python3/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
