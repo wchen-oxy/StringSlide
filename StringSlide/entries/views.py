ca4221d3b4a6405907f99ca5ce5ecbf50a3fb80a
William S Chen <blackjamx5@hotmail.com>
2019-04-23 22:49:44 -0700

    Added more basic Template

--- StringSlide/entries/views.py
+++ StringSlide/entries/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+
+import random
 from .models import Guitar
 
 # Create your views here.
@@ -7,5 +10,10 @@ def index(request):
     return HttpResponse("Hello, world. You're at the entries")
 
 def home(request):
+    guitar_id = random.randint(0, 999)
+    chosen = get_object_or_404(Guitar, pk=guitar_id)
+    picked = Guitar.objects.get(guitar_id=1)
+    print(chosen)
+    print(picked)
     
-    return render(request, 'home.html')
+    return render(request, 'home.html', {'place': picked.guitar_name})
