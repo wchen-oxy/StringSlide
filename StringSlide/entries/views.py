781067a923825319a2664ec82b139ba7c8fc7f3f
William S Chen <blackjamx5@hotmail.com>
2019-04-24 12:18:25 -0700

    Added html/css

--- StringSlide/entries/views.py
+++ StringSlide/entries/views.py
@@ -10,10 +10,23 @@ def index(request):
     return HttpResponse("Hello, world. You're at the entries")
 
 def home(request):
-    guitar_id = random.randint(0, 999)
-    chosen = get_object_or_404(Guitar, pk=guitar_id)
-    picked = Guitar.objects.get(guitar_id=1)
-    print(chosen)
-    print(picked)
+
+    guitar = Guitar.objects.get(guitar_id=1)
+    guitar2 = Guitar.objects.get(guitar_id=2)
+    guitar3 = Guitar.objects.get(guitar_id=3)
+
+
     
-    return render(request, 'home.html', {'place': picked.guitar_name})
+    return render(request, 'home.html', {'guitar_id': guitar.guitar_id,
+                                         'guitar_model':guitar.guitar_model,
+                                         'guitar_name': guitar.guitar_name,
+
+                                         'guitar_id2': guitar2.guitar_id,
+                                         'guitar_model2': guitar2.guitar_model,
+                                         'guitar_name2': guitar2.guitar_name,
+
+                                         'guitar_id3': guitar3.guitar_id,
+                                         'guitar_model3': guitar3.guitar_model,
+                                         'guitar_name3': guitar3.guitar_name,
+
+                                         })
